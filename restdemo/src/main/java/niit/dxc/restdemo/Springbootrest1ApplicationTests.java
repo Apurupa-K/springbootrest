e3b76b55d88e65bac822553394e3ad426ee2f712
apurupa <apurupa@LAPTOP-SL8NQJ86>
2021-04-30 09:48:02 +0530

    initial commit

---
+++ restdemo/src/main/java/niit/dxc/restdemo/Springbootrest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package niit.dxc.restdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootrest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootrest1Application.class, args);
+	}
+
+}
